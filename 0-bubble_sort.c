5b1251c3e5c2910f5eb035e787741b5240334554
jlassi1 <khawlajlassi1990@gmail.com>
2020-06-15 11:10:24 +0100

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,9 +13,9 @@ void bubble_sort(int *array, size_t size)
 size_t i, j;
 int tmp;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size ; i++)
 	{
-		for (j = 0; j < size + 1 - i; j++)
+		for (j = 1; j < (size - 1 - i); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
