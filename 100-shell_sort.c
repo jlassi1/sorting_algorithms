f6f1fd6a14624613b59c72601caf92b060790bd6
jlassi1 <khawlajlassi1990@gmail.com>
2020-06-17 13:31:59 +0100

    100-shell_sort.c

---
+++ 100-shell_sort.c
@@ -0,0 +1,47 @@
+#include"sort.h"
+
+/**
+ * shell_sort - sorts an arry in a shell algorithm
+ * @array: an array
+ * @size: its size
+ *
+ * Return: void
+ */
+
+void shell_sort(int *array, size_t size)
+{
+size_t gap = 1;
+size_t i, j;
+
+if (size < 2)
+return;
+while (gap < size / 3)
+{
+gap = gap * 3 + 1;
+}
+while (gap > 0)
+{
+for (i = 0; i < size - 1; i++)
+{
+for (j = i; j < size; j += gap)
+{
+if (array[i] > array[j])
+swap(&array[i], &array[j]);
+}
+}
+print_array(array, size);
+gap = (gap - 1) / 3;
+}
+for (i = 0; i < size - 1; i++)
+{
+if (array[i + 1] < array[i])
+{
+swap(&array[i], &array[i + 1]);
+for (j =  i; j > 0; j--)
+{
+if (array[j] > array[j + 1])
+swap(&array[j], &array[j + 1]);
+}
+}
+}
+}
--- sort.h
+++ sort.h
@@ -22,10 +22,12 @@ typedef struct listint_s
 /*---------PROTOTYPE--------*/
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void swap(int *a, int *b);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 
 
