487df354ba2630cec54180b15bedf12848fe4b48
jlassi1 <khawlajlassi1990@gmail.com>
2020-06-17 21:44:40 +0100

    101-O

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ *cocktail_sort_list- sort list with cocktail sort algorithm
+ *
+ *@list: doubly linked list
+ *
+ *Return: void
+ */
+void cocktail_sort_list(listint_t **list)
+{
+
+}
