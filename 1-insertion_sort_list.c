cdde8d5a1dbc77133a302ca734500cc530bf3dd7
jlassi1 <khawlajlassi1990@gmail.com>
2020-06-18 09:10:41 +0100

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,7 @@
 void insertion_sort_list(listint_t **list)
 {
 listint_t *curr = NULL, *tmp = *list, *head;
-if (!list || !*list || !(*list)->next)
+if (!list || !(*list) || !(*list)->next)
 	return;
 
 head = (*list)->next;
